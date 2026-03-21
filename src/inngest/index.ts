e8f5661779445f35c676a042a668ba9e4e1cfecf
Prashant Kumar <prashant0kumar101@gmail.com>
2026-03-21 23:05:09 +0530

    fix the inngest file

--- src/inngest/index.ts
+++ src/inngest/index.ts
@@ -1,73 +1,75 @@
 import { Inngest } from "inngest";
 import User from "../models/user.model";
 
-export const inngest = new Inngest({ id: "my-app" });
+export const inngest = new Inngest({
+  id: "my-app",
+  signingKey: process.env.INNGEST_SIGNING_KEY,
+  eventKey: process.env.INNGEST_EVENT_KEY,
+});
 
 const syncUserCreation = inngest.createFunction(
-    { id: "sync-user-creation", triggers: [{ event: "clerk/user.created" }] },
+  { id: "sync-user-creation", triggers: [{ event: "clerk/user.created" }] },
 
-    async ({ event }) => {
-        const { id, first_name, last_name, email_addresses, image_url } = event.data;
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
 
-        const email = email_addresses?.[0]?.email_address;
+    const email = email_addresses?.[0]?.email_address;
 
-        if (!email) {
-            throw new Error("Email not found in Clerk payload");
-        }
-
-        let username = email.split("@")[0];
-
-        const existingUser = await User.findOne({ username });
-        if (existingUser) {
-            username = username + Math.floor(Math.random() * 1000);
-        }
-
-        const userData = {
-            _id: id,
-            email,
-            full_name: `${first_name || ""} ${last_name || ""}`,
-            username,
-            profile_picture: {
-                url: image_url,
-                fileId: "",
-            },
-            bio: "write your bio here",
-        };
-
-        await User.create(userData);
+    if (!email) {
+      throw new Error("Email not found in Clerk payload");
     }
+
+    let username = email.split("@")[0];
+
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      username = username + Math.floor(Math.random() * 1000);
+    }
+
+    const userData = {
+      _id: id,
+      email,
+      full_name: `${first_name || ""} ${last_name || ""}`,
+      username,
+      profile_picture: {
+        url: image_url,
+        fileId: "",
+      },
+      bio: "write your bio here",
+    };
+
+    await User.create(userData);
+  },
 );
 
 const syncUserUpdation = inngest.createFunction(
-    { id: "sync-user-updation", triggers: [{ event: "clerk/user.updated" }] },
+  { id: "sync-user-updation", triggers: [{ event: "clerk/user.updated" }] },
 
-    async ({ event }) => {
-        const { id, first_name, last_name, email_addresses, image_url } = event.data;
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
 
-        const email = email_addresses?.[0]?.email_address;
+    const email = email_addresses?.[0]?.email_address;
 
-        await User.findOneAndUpdate(
-            { _id: id },
-            {
-                email,
-                full_name: `${first_name || ""} ${last_name || ""}`,
-                profile_picture: image_url,
-            }
-        );
-    }
+    await User.findOneAndUpdate(
+      { _id: id },
+      {
+        email,
+        full_name: `${first_name || ""} ${last_name || ""}`,
+        profile_picture: image_url,
+      },
+    );
+  },
 );
 
 const syncUserDeletion = inngest.createFunction(
-    { id: "sync-user-deletion", triggers: [{ event: "clerk/user.deleted" }] },
+  { id: "sync-user-deletion", triggers: [{ event: "clerk/user.deleted" }] },
 
-    async ({ event }) => {
-        const { id } = event.data;
-        await User.findOneAndDelete({ _id: id });
-    }
+  async ({ event }) => {
+    const { id } = event.data;
+    await User.findOneAndDelete({ _id: id });
+  },
 );
 
-export const functions = [
-    syncUserCreation,
-    syncUserUpdation,
-    syncUserDeletion,
-];
+export const functions = [syncUserCreation, syncUserUpdation, syncUserDeletion];
